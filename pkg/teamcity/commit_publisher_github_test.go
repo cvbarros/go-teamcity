324c34f59d866b6f46e02e152a40477a91dbda85
carlos.barros <carlosvitorbarros@gmail.com>
2018-05-22 16:32:24 +0200

    Implement options Unmarshalling for FeatureCommitStatusPublisher

--- pkg/teamcity/commit_publisher_github_test.go
+++ pkg/teamcity/commit_publisher_github_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const githubHost = "https://api.github.com"
+
 func TestFeatureCommitPublisher_GithubRequiredProperties(t *testing.T) {
 	t.Run("AuthenticationType Required", func(t *testing.T) {
 		opt := teamcity.StatusPublisherGithubOptions{}
@@ -28,24 +30,24 @@ func TestFeatureCommitPublisher_GithubRequiredProperties(t *testing.T) {
 
 func TestFeatureCommitPublisher_GithubAuthenticationPassword(t *testing.T) {
 	t.Run("Username Required", func(t *testing.T) {
-		opt := teamcity.StatusPublisherGithubOptions{AuthenticationType: "password", Host: "https://api.github.com", Password: "1234"}
+		opt := teamcity.StatusPublisherGithubOptions{AuthenticationType: "password", Host: githubHost, Password: "1234"}
 		_, err := teamcity.NewFeatureCommitStatusPublisherGithub(opt)
 		require.EqualError(t, err, "username/password required for auth type 'password'")
 	})
 	t.Run("Password Required", func(t *testing.T) {
-		opt := teamcity.StatusPublisherGithubOptions{AuthenticationType: "password", Host: "https://api.github.com", Username: "bob"}
+		opt := teamcity.StatusPublisherGithubOptions{AuthenticationType: "password", Host: githubHost, Username: "bob"}
 		_, err := teamcity.NewFeatureCommitStatusPublisherGithub(opt)
 		require.EqualError(t, err, "username/password required for auth type 'password'")
 	})
 
 	t.Run("Correct Properties", func(t *testing.T) {
 		assert := assert.New(t)
-		opt := teamcity.NewCommitStatusPublisherGithubOptionsPassword("https://api.github.com", "bob", "1234")
+		opt := teamcity.NewCommitStatusPublisherGithubOptionsPassword(githubHost, "bob", "1234")
 
 		actual := opt.Properties().Map()
 
 		assert.Equal("githubStatusPublisher", actual["publisherId"])
-		assert.Equal("https://api.github.com", actual["github_host"])
+		assert.Equal(githubHost, actual["github_host"])
 		assert.Equal("password", actual["github_authentication_type"])
 		assert.Equal("bob", actual["github_username"])
 		assert.Equal("1234", actual["secure:github_password"])
@@ -54,20 +56,56 @@ func TestFeatureCommitPublisher_GithubAuthenticationPassword(t *testing.T) {
 
 func TestFeatureCommitPublisher_GithubAuthenticationToken(t *testing.T) {
 	t.Run("AccessToken Required", func(t *testing.T) {
-		opt := teamcity.StatusPublisherGithubOptions{AuthenticationType: "token", Host: "https://api.github.com"}
+		opt := teamcity.StatusPublisherGithubOptions{AuthenticationType: "token", Host: githubHost}
 		_, err := teamcity.NewFeatureCommitStatusPublisherGithub(opt)
 		require.EqualError(t, err, "accesstoken required for auth type 'token'")
 	})
 
 	t.Run("Correct Properties", func(t *testing.T) {
 		assert := assert.New(t)
-		opt := teamcity.NewCommitStatusPublisherGithubOptionsToken("https://api.github.com", "1234")
+		opt := teamcity.NewCommitStatusPublisherGithubOptionsToken(githubHost, "1234")
 
 		actual := opt.Properties().Map()
 
 		assert.Equal("githubStatusPublisher", actual["publisherId"])
-		assert.Equal("https://api.github.com", actual["github_host"])
+		assert.Equal(githubHost, actual["github_host"])
 		assert.Equal("token", actual["github_authentication_type"])
 		assert.Equal("1234", actual["secure:github_access_token"])
+	})
+}
+
+func TestFeatureCommitPublisher_GithubFromProperties(t *testing.T) {
+	props := teamcity.NewProperties([]*teamcity.Property{
+		teamcity.NewProperty("github_host", githubHost),
+		teamcity.NewProperty("github_authentication_type", "password"),
+		teamcity.NewProperty("github_username", "bob"),
+		teamcity.NewProperty("secure:github_password", "1234"),
+		teamcity.NewProperty("secure:github_access_token", "1234"),
+	}...)
+
+	t.Run("From Password AuthType", func(t *testing.T) {
+		assert := assert.New(t)
+
+		actual, err := teamcity.CommitStatusPublisherGithubOptionsFromProperties(props)
+
+		require.NoError(t, err)
+		assert.Equal("password", actual.AuthenticationType)
+		assert.Equal(githubHost, actual.Host)
+		assert.Equal("bob", actual.Username)
+		assert.Empty(actual.Password)
+		assert.Empty(actual.AccessToken)
+	})
+
+	t.Run("From Token AuthType", func(t *testing.T) {
+		assert := assert.New(t)
+		props.AddOrReplaceValue("github_authentication_type", "token")
+		actual, err := teamcity.CommitStatusPublisherGithubOptionsFromProperties(props)
+
+		require.NoError(t, err)
+		assert.Equal("token", actual.AuthenticationType)
+		assert.Equal(githubHost, actual.Host)
+		assert.Empty(actual.Username)
+		assert.Empty(actual.Password)
+		assert.Empty(actual.AccessToken)
 	})
 }
