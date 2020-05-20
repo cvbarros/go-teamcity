496981173ad6af96bdebf77846a06058ff05602d
Emily Li <eli@guidewire.com>
2020-05-20 11:34:56 -0400

    Refactor to a generic RoleAssignmentService.

--- teamcity/role_assignment_test.go
+++ teamcity/role_assignment_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGroupRoleAssignment_GlobalSysAdminAssign(t *testing.T) {
+func TestRoleAssignment_AssignGlobalSysAdminToGroup(t *testing.T) {
 	client := setup()
 
 	newGroup, _ := teamcity.NewGroup("TESTGROUPKEY", "Test Group Name", "Test Group Description")
@@ -18,14 +18,14 @@ func TestGroupRoleAssignment_GlobalSysAdminAssign(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, actualGroup)
 
-	createdGroupRoleAssignment, err := client.GroupRoleAssignments.Assign(newGroupRoleAssignment)
+	createdGroupRoleAssignment, err := client.RoleAssignments.AssignToGroup(newGroupRoleAssignment)
 	require.NoError(t, err)
 	require.NotNil(t, createdGroupRoleAssignment)
 	assert.NotEmpty(t, createdGroupRoleAssignment.RoleID)
 	assert.NotEmpty(t, createdGroupRoleAssignment.Scope)
 	assert.NotEmpty(t, createdGroupRoleAssignment.Href)
 
-	groupRoleAssignments, err := client.GroupRoleAssignments.GetAll(newGroup)
+	groupRoleAssignments, err := client.RoleAssignments.GetAllForGroup(newGroup)
 	require.NoError(t, err)
 	assert.Equal(t, 1, len(groupRoleAssignments))
 	actualGroupRoleAssignmentReference := groupRoleAssignments[0]
@@ -33,7 +33,7 @@ func TestGroupRoleAssignment_GlobalSysAdminAssign(t *testing.T) {
 	assert.Equal(t, createdGroupRoleAssignment.Scope, actualGroupRoleAssignmentReference.Scope)
 	assert.Equal(t, createdGroupRoleAssignment.Href, actualGroupRoleAssignmentReference.Href)
 
-	actualGroupRoleAssignmentReference2, err := client.GroupRoleAssignments.Get(newGroupRoleAssignment)
+	actualGroupRoleAssignmentReference2, err := client.RoleAssignments.GetForGroup(newGroupRoleAssignment)
 	require.NoError(t, err)
 	assert.Equal(t, createdGroupRoleAssignment.RoleID, actualGroupRoleAssignmentReference2.RoleID)
 	assert.Equal(t, createdGroupRoleAssignment.Scope, actualGroupRoleAssignmentReference2.Scope)
@@ -43,7 +43,7 @@ func TestGroupRoleAssignment_GlobalSysAdminAssign(t *testing.T) {
 	cleanUpGroup(t, client, actualGroup.Key)
 }
 
-func TestGroupRoleAssignment_Assign(t *testing.T) {
+func TestRoleAssignment_AssignToGroup(t *testing.T) {
 	client := setup()
 
 	parent, _ := teamcity.NewProject("ParentProject", "Parent Project", "")
@@ -61,14 +61,14 @@ func TestGroupRoleAssignment_Assign(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, actualGroup)
 
-	createdGroupRoleAssignment, err := client.GroupRoleAssignments.Assign(newGroupRoleAssignment)
+	createdGroupRoleAssignment, err := client.RoleAssignments.AssignToGroup(newGroupRoleAssignment)
 	require.NoError(t, err)
 	require.NotNil(t, createdGroupRoleAssignment)
 	assert.NotEmpty(t, createdGroupRoleAssignment.RoleID)
 	assert.NotEmpty(t, createdGroupRoleAssignment.Scope)
 	assert.NotEmpty(t, createdGroupRoleAssignment.Href)
 
-	groupRoleAssignments, err := client.GroupRoleAssignments.GetAll(newGroup)
+	groupRoleAssignments, err := client.RoleAssignments.GetAllForGroup(newGroup)
 	require.NoError(t, err)
 	assert.Equal(t, 1, len(groupRoleAssignments))
 	actualGroupRoleAssignmentReference := groupRoleAssignments[0]
@@ -76,7 +76,7 @@ func TestGroupRoleAssignment_Assign(t *testing.T) {
 	assert.Equal(t, createdGroupRoleAssignment.Scope, actualGroupRoleAssignmentReference.Scope)
 	assert.Equal(t, createdGroupRoleAssignment.Href, actualGroupRoleAssignmentReference.Href)
 
-	actualGroupRoleAssignmentReference2, err := client.GroupRoleAssignments.Get(newGroupRoleAssignment)
+	actualGroupRoleAssignmentReference2, err := client.RoleAssignments.GetForGroup(newGroupRoleAssignment)
 	require.NoError(t, err)
 	assert.Equal(t, createdGroupRoleAssignment.RoleID, actualGroupRoleAssignmentReference2.RoleID)
 	assert.Equal(t, createdGroupRoleAssignment.Scope, actualGroupRoleAssignmentReference2.Scope)
@@ -87,7 +87,7 @@ func TestGroupRoleAssignment_Assign(t *testing.T) {
 	cleanUpProject(t, client, "ParentProject")
 }
 
-func TestGroupRoleAssignment_Unassign(t *testing.T) {
+func TestRoleAssignment_UnassignFromGroup(t *testing.T) {
 	client := setup()
 
 	parent, _ := teamcity.NewProject("ParentProject", "Parent Project", "")
@@ -105,22 +105,22 @@ func TestGroupRoleAssignment_Unassign(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, actualGroup)
 
-	createdGroupRoleAssignment, err := client.GroupRoleAssignments.Assign(newGroupRoleAssignment)
+	createdGroupRoleAssignment, err := client.RoleAssignments.AssignToGroup(newGroupRoleAssignment)
 	require.NoError(t, err)
 	require.NotNil(t, createdGroupRoleAssignment)
 	assert.NotEmpty(t, createdGroupRoleAssignment.RoleID)
 	assert.NotEmpty(t, createdGroupRoleAssignment.Scope)
 	assert.NotEmpty(t, createdGroupRoleAssignment.Href)
 
-	err = client.GroupRoleAssignments.Unassign(newGroupRoleAssignment)
+	err = client.RoleAssignments.UnassignFromGroup(newGroupRoleAssignment)
 	require.NoError(t, err)
 
-	groupRoleAssignments, err := client.GroupRoleAssignments.GetAll(newGroup)
+	groupRoleAssignments, err := client.RoleAssignments.GetAllForGroup(newGroup)
 	require.NoError(t, err)
 	assert.Equal(t, 0, len(groupRoleAssignments))
 
 	// The Role has been unassigneded, so expect error, and message to contain 404 (NOT FOUND)
-	_, err = client.GroupRoleAssignments.Get(newGroupRoleAssignment)
+	_, err = client.RoleAssignments.GetForGroup(newGroupRoleAssignment)
 	require.Error(t, err)
 	assert.Contains(t, err.Error(), "404")
 
